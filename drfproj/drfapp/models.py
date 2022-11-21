e1998097520da825b8d3476eb0bfec0e835bf6db
dawmro <dawidmrotek@protonmail.ch>
2022-11-21 09:22:05 -0800

    create student model

--- drfproj/drfapp/models.py
+++ drfproj/drfapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.Charfield(max_length=100)
+    age = models.IntegerField()
+    description = models.TextField()
+    date_enroled = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
