899e0c4d529d1b5854c72e1393ed32655393d314
dawmro <dawidmrotek@protonmail.ch>
2022-11-21 07:52:18 -0800

    add home view

--- drfproj/drfproj/urls.py
+++ drfproj/drfproj/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import TesView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TesView.as_view(), name='test')
 ]
