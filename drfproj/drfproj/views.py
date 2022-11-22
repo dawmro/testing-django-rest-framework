919e41ea98c3643a55f010b3abc651bbce5968ec
dawmro <dawidmrotek@protonmail.ch>
2022-11-22 05:08:08 -0800

    add authentication

--- drfproj/drfproj/views.py
+++ drfproj/drfproj/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from drfapp.serializers import StudentSerializer
 from drfapp.models import Student
 
 
 class TesView(APIView):
+
+    permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
         query_set = Student.objects.all()
